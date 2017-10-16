6c8467f05671263e362945a9a348643f979d7040
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-10-16 16:02:45 +0100

    add clue parsing in init function

--- grid.py
+++ grid.py
@@ -1,3 +1,14 @@
+import numpy as np
+from square import Square
+
 class Grid(object):
-	def __init__(self, size, clues):
-		pass
+	def __init__(self, clues):
+		self.clues = clues
+		self.width = len(clues)
+		self.grid = []
+		for i in range(self.width):
+			temp_row = []
+			for j in range(self.width):
+				temp_row.append(Square(clues[i][j]))
+			self.grid.append(temp_row)
+			
