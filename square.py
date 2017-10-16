859a9ff3cc1488c15157be7ac369d65bc871f5be
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-10-16 16:02:30 +0100

    add functions to change sides values

--- square.py
+++ square.py
@@ -4,4 +4,13 @@ class Square(object):
 		self.top = 0
 		self.bottom = 0
 		self.left = 0
-		left.right = 0
+		self.right = 0
+
+	def shade_top(self):
+		self.top = 1
+	def shade_bottom(self):
+		self.bottom = 1
+	def shade_right(self):
+		self.right = 1
+	def shade_left(self):
+		self.left = 1
