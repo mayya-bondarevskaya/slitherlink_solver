a98e61dac017b71103199a0626890e4c28a7d776
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-10-16 17:26:06 +0100

    add functions to shade cells in grid, and a function to find diagonally locate 3's and shade accordingly

--- grid.py
+++ grid.py
@@ -11,4 +11,38 @@ class Grid(object):
 			for j in range(self.width):
 				temp_row.append(Square(clues[i][j]))
 			self.grid.append(temp_row)
-			
+
+	def find_diag_threes(self):
+		for i in range(self.width - 1):
+			for j in range(self.width - 1):
+				if (self.grid[i][j].clue == 3) and (self.grid[i+1][j+1] == 3):
+					self.shade_left(i, j)
+					self.shade_top(i, j)
+					self.shade_right(i + 1, j + 1)
+					self.shade_bottom(i + 1, j + 1)
+			for j in range(1, self.width):
+				if (self.grid[i][j].clue == 3) and (self.grid[i+1][j-1] == 3):
+					self.shade_top(i, j)
+					self.shade_right(i, j)
+					self.shade_left(i+1, j-1)
+					self.shade_bottom(i+1, j-1)
+
+	def shade_left(self, row, col):
+		self.grid[row][col].shade_left()
+		if col != 0:
+			self.grid[row][col - 1].shade_right()
+
+	def shade_right(self, row, col):
+		self.grid[row][col].shade_right()
+		if col != self.width - 1:
+			self.grid[row][col + 1].shade_left()
+
+	def shade_top(self, row, col):
+		self.grid[row][col].shade_top()
+		if row != 0:
+			self.grid[row - 1][col].shade_bottom()
+
+	def shade_bottom(self, row, col):
+		self.grid[row][col].shade_bottom()
+		if row != self.width - 1:
+			self.grid[row + 1][col].shade_top()
