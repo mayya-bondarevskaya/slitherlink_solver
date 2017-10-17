5d16055136373c37a4c29f5ddf643f29a4195189
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-10-17 11:20:57 +0100

    fix find_diag_threes function

--- grid.py
+++ grid.py
@@ -14,23 +14,40 @@ class Grid(object):
 
 	def find_diag_threes(self):
 		for i in range(self.width - 1):
-			print("Starting work on row", i)
-			for j in range(self.width - 1):
-				print(self.grid[i][j].clue, self.grid[i+1][j+1].clue)
+			if (self.grid[i][0].clue == 3) and (self.grid[i + 1][1].clue == 3):
+				self.shade_left(i, 0)
+				self.shade_top(i, 0)
+				self.shade_right(i + 1, 1)
+				self.shade_bottom(i + 1, 1)
+			if (self.grid[i][self.width-1].clue == 3) and (self.grid[i+1][self.width-2].clue ==3):
+				self.shade_right(i, self.width-1)
+				self.shade_top(i, self.width-1)
+				self.shade_bottom(i+1, self.width-2)
+				self.shade_left(i+1, self.width-2)
+			for j in range(1, self.width - 1):
 				if (self.grid[i][j].clue == 3) and (self.grid[i+1][j+1].clue == 3):
-					print("Diagonal 3's found!")
 					self.shade_left(i, j)
 					self.shade_top(i, j)
 					self.shade_right(i + 1, j + 1)
 					self.shade_bottom(i + 1, j + 1)
-			for j in range(1, self.width):
-				print(self.grid[i][j].clue, self.grid[i+1][j-1].clue)
 				if (self.grid[i][j].clue == 3) and (self.grid[i+1][j-1].clue == 3):
-					print("Diagonal 3's found!")
 					self.shade_top(i, j)
 					self.shade_right(i, j)
 					self.shade_left(i+1, j-1)
 					self.shade_bottom(i+1, j-1)
+
+	def find_nearby_threes(self):
+		for i in range(self.width - 1):
+			for j in range(self.width - 1):
+				if self.grid[i][j].clue == 3:
+					if self.grid[i][j + 1] == 3:
+						self.shade_left(i, j)
+						self.shade_right(i, j)
+						self.shade_right(i, j + 1)
+					if self.grid[i + 1][j] == 3:
+						self.shade_top(i, j)
+						self.shade_bottom(i, j)
+						self.shade_bottom(i + 1, j)
 
 	def shade_left(self, row, col):
 		self.grid[row][col].shade_left()
@@ -51,3 +68,23 @@ class Grid(object):
 		self.grid[row][col].shade_bottom()
 		if row != self.width - 1:
 			self.grid[row + 1][col].shade_top()
+
+	def cross_left(self, row, col):
+		self.grid[row][col].cross_left()
+		if col != 0:
+			self.grid[row][col - 1].cross_right()
+
+	def cross_right(self, row, col):
+		self.grid[row][col].cross_right()
+		if col != self.width - 1:
+			self.grid[row][col + 1].cross_left()
+
+	def cross_top(self, row, col):
+		self.grid[row][col].cross_top()
+		if row != 0:
+			self.grid[row - 1][col].cross_bottom()
+
+	def cross_bottom(self, row, col):
+		self.grid[row][col].cross_bottom()
+		if row != self.width - 1:
+			self.grid[row + 1][col].cross_top()
