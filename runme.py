fec45e39d49405cc4a40e6cdfadca1876b414390
Mayya Bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-10-24 16:01:53 +0100

    change example to being a more involved example

--- runme.py
+++ runme.py
@@ -2,12 +2,22 @@
  # -*- coding: utf-8 -*-
 from grid import Grid
 
-clues = [[None, 2, 2, None, 3],
-		 [None, 2, None, None, None],
-		 [3, 2, None, None, None],
-		 [1, None, None, 0, 3],
-		 [None, 2, 2, 3, None]]
+clues = [[None, 3, 2, None, None, 3, None, None, 3, None, None, None, None, 2, 2],
+		 [3, 1, None, None, None, None, 1, 3, 2, 2, None, 3, None, 3, None],
+		 [None, None, 1, 1, None, 2, None, 1, 2, None, 3, None, 2, None, 1],
+		 [2, 3, None, 2, None, None, None, None, None, 1, None, 1, None, None, 2],
+		 [2, None, 2, None, None, 2, 2, None, None, None, 3, None, None, 2, None],
+		 [3, None, None, 3, None, 3, 1, 2, None, 2, 2, None, 1, 2, None],
+		 [None, None, None, 2, None, None, None, 0, None, None, None, None, None, None, None],
+		 [None, None, None, None, 3, 1, 3, 2, None, None, 2, None, 1, None, 2],
+		 [None, None, None, 2, None, None, 3, None, None, 3, None, 3, None, 3, None],
+		 [None, 3, None, None, None, 1, None, None, None, 2, None, None, 1, 3, None],
+		 [2, 3, None, 1, None, None, 2, None, 2, None, None, None, None, 2, 2],
+		 [None, 2, 3, None, 1, None, 1, None, 1, None, None, 2, None, None, 2],
+		 [None, 2, 2, 1, None, None, None, None, 3, None, None, None, 2, 1, None],
+		 [None, 2, 3, None, 3, 1, None, 1, 2, None, None, 2, 2, 2, None],
+		 [3, 2, 1, None, 2, 2, None, 3, 3, None, 2, 2, None, None, None]]
 
 g = Grid(clues)
-g.run(20)
+g.run(200)
 g.print_grid()
